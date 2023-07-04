34b83b0aa192d20addf0abab5d3e627c122ae330
Nihad Soltanov <mr.nihadsoltanov@gmail.com>
2023-07-04 23:34:57 +0400

    Photo Stock Service save and delete operations

---
+++ Services/SellingCourse.Service.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,36 @@
+﻿using FreeCourses.Shared.ControllerBases;
+using FreeCourses.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SellingCourse.Service.PhotoStock.Dtos;
+
+namespace SellingCourse.Service.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if(photo !=null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+                using var stream = new FileStream(path, FileMode.Create);
+                var returnPath = "photos/" + photo.FileName;
+                PhotoDto photoDto= new PhotoDto() { Url=returnPath};
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("photo is empty", 400));
+        }
+
+        [HttpDelete]
+        public IActionResult DeletePhoto(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+            if (!System.IO.File.Exists(path)) return CreateActionResultInstance(Response<NoContent>.Fail("photo can't find",404));
+            System.IO.File.Delete(path);
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+    }
+}
---
+++ Services/SellingCourse.Service.PhotoStock/Dtos/PhotoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SellingCourse.Service.PhotoStock.Dtos
+{
+    public class PhotoDto
+    {
+        public string Url { get; set; }
+    }
+}

a0d02faebb041e722c1301424d1fa72fa4acd949
Nihad Soltanov <mr.nihadsoltanov@gmail.com>
2023-07-06 18:19:34 +0400

    add Basket Dtos and configuration Redis connection

---
+++ Services/Basket/SellingCourse.Service.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SellingCourse.Service.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        public int  Quantity { get; set; }
+        public string CourseID { get; set; }
+        public string CourseName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Services/Basket/SellingCourse.Service.Basket/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.Extensions.Configuration;
+using SellingCourse.Service.Basket.Settings;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json")
+    .Build();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<RedisSettings>(configuration.GetSection("RedisSettings"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
